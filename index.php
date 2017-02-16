a2e4186455598aecaa0bae825508c31e3a61f241
beltjun26 <beltjunabisado@gmail.com>
2017-02-16 22:21:51 +0800

    web structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HSIS Login</title>
+	<script type="text/javascript"></script>
+</head>
+<body>
+	<h1>HS Information System</h1>
+	<form>
+		<label>Username: </label>
+		<input type="text" name="username"/>
+		<label>Password: </label>
+		<input type="password" name="password">
+	</form>
+
+</body>
+</html>
